ef887352a7b773fb5e826d747ee23a2ffa5306e2
Karim Moradi <kmoradi.ai@gmail.com>
2023-03-14 22:06:54 +0100

    encapsulation in py

--- encapsulation.py
+++ encapsulation.py
@@ -12,13 +12,14 @@ a_class = AClass()
 
 a_class.name = 'Charim'
 a_class._name = 'Javaad'
-a_class.__name = 'Rassol'
+a_class.__name = 'Rassol'  # It just adds another attribute.
+
 
 print(a_class.__dict__)
 # {'name': 'Charim',
 # '_name': 'Javaad',
 # '_AClass__name': 'Masoud',
-# '__name': 'Rassol'}  NOTE: You just added another attribute here
+# '__name': 'Rassol'}
 
 a_class.update_name('Rasoul')
 print(a_class.__dict__)
@@ -26,3 +27,18 @@ print(a_class.__dict__)
 # '_name': 'Javaad',
 # '_AClass__name': 'Rasoul',
 # '__name': 'Rassol'}
+
+xyz = 1,000,000
+# x y z = 1000 2000 3000
+x,y,z = 1000, 2000, 3000
+x_y_z = 1,000,000
+
+print(xyz)
+print(x, y, z)
+print(x_y_z)
+
+x, *_, y = 1, 2, 3, 4, 7, 5, 6
+
+print(x)
+print(y)
+print(_)
---
+++ lnweakref.py
@@ -0,0 +1,32 @@
+import os
+import time
+
+import psutil
+import gc
+import requests
+
+cars = []
+
+
+class Memory:
+    symbols = []
+
+    def __init__(self, sym):
+        self.symbols.append(sym)
+
+    def get_symbol(self):
+        return self.symbols
+
+
+print(len(gc.get_objects()))
+mem = Memory(1)
+del mem
+
+mem2 = Memory(2)
+
+for i in range(100):
+    Memory(i)
+
+gc.collect()
+print(len(gc.get_objects()))
+print(mem2.get_symbol())
