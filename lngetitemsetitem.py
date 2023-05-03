7bd4da16c6dd976e395ad2fb1962692ee7d733dc
Karim Moradi <kmoradi.ai@gmail.com>
2023-05-03 11:29:06 +0200

    practice getitem setitem magic method

---
+++ lngetitemsetitem.py
@@ -0,0 +1,19 @@
+class Text:
+    def __init__(self, names: str):
+        self.names = names.split(' ')
+
+    def __getitem__(self, item):
+        print(self.names.index(item))
+        return self.names.index(item)
+
+    def __setitem__(self, key, value):
+        while len(self.names) <= key - 1:
+            self.names.append("temp")
+        self.names.append(value)
+        print(key, ':\t ', value)
+
+
+my_text = Text("Karim Javad Rasoul")
+javad = my_text["Rasoul"]
+my_text[4] = "Ali"
+print(my_text.names)
