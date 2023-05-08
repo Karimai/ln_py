739e9068bc53b6856203c589e631dc0eaadc21e4
Karim Moradi <kmoradi.ai@gmail.com>
2023-05-08 16:56:11 +0200

    practice itertools

--- lnittertools.py
+++ lnittertools.py
@@ -35,3 +35,49 @@ print(list(squares))
 # Combinations: all the different ways you can group a certain number of items. Order does NOT matter
 # (a, b) == (b, a), so they can not appear both at the same time.
 # Permutation: A combination when order does matter
+
+
+def second():
+    return 2
+
+
+def stop():
+    return 5
+
+
+results = itertools.islice(range(10), second(), stop(), 2)
+print(list(results))
+
+for book in ["book1.txt", "book2.txt"]:
+    with open(book, "r") as fd:
+        for line in itertools.islice(fd, 3):
+            print(line, end="")
+
+
+nominates = ["Ali", "Bart", "Femke", "Pieter", "Karim"]
+accepted = [True, True, False, False, True]
+for accepted_candidate in itertools.compress(nominates, accepted):
+    print(accepted_candidate)
+
+for accepted_candidate in itertools.compress(data=nominates, selectors=accepted):
+    print(accepted_candidate)
+
+
+# the values should be sorted beforehand.
+people = [
+    {
+        "name": "Karim",
+        "age": 41,
+    },
+    {"name": "Javad", "age": 41},
+    {"name": "Bart", "age": 41},
+    {"name": "John", "age": 43},
+    {"name": "messi", "age": 43},
+]
+
+grouped_people = itertools.groupby(people, lambda person: person["age"])
+
+for g in grouped_people:
+    print(g[0])
+    for grouped in g[1]:
+        print(grouped)
--- lnunpacking.py
+++ lnunpacking.py
@@ -1,13 +1,14 @@
 def multiply(*args):
     print(type(args))
 
+
 def splat(**kwargs):
     for arg in kwargs.items():
         print(arg)
+
 
 vars = [2, 3, 4]
 multiply(*vars)
 
 kwargs = {"Name": "Karim", "Age": 41}
 splat(**kwargs)
-
